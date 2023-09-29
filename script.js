8a2d62846672e1e7817949133039b66f07b8db63
lorenzoCiaravola <101358212+thezarathustra01@users.noreply.github.com>
2023-09-29 17:17:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript code for any interactive features can go here
+// For example, you can add form validation or dynamic content loading.
+// This is a placeholder script.
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add your JavaScript code here
+    // For example, form validation or handling form submission.
+});
